9c2a591ee7343335f6e20783a77a3d4d983ff0d6
Ajay Singh Bartwal <142952841+TechajayBT@users.noreply.github.com>
2026-08-31 13:45:40 +0530

    Initial Commit

---
+++ src/main/java/com/example/BuildingKafkaFromScratch/BuildingKafkaFromScratchApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuildingKafkaFromScratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingKafkaFromScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingKafkaFromScratchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BuildingKafkaFromScratch/BuildingKafkaFromScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuildingKafkaFromScratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingKafkaFromScratchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
